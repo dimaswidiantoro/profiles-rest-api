76919a4fd4eb5ba9351804e987b95ea3fc512582
dimaswidiantoro <tjitrodimedjo@gmail.com>
2018-11-04 20:45:12 +0100

    add userprofile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+
 # Create your models here.
 class UserProfileManager(BaseUserManager):
     """Help django to identify our custom user model"""
